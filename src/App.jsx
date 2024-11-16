7f92c35ef11914bb50db53100be5427c85fddc0c
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-11-16 14:01:19 +0530

    search funtionality is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 import AddTodo from "./components/AddTodo";
 import { useSelector} from "react-redux";
 import Todos from "./components/Todos.jsx";
 import './App.css'
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
   const todos = useSelector((state) => state.todos);
-  console.log("todos :: ",todos);
-  
+
+  // Search
+  const filteredTodos = todos.filter((todo) =>
+    todo.text.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className="min-h-screen bg-[#DCF2F1] xl:py-8 py-4 ">
       <div className="font-bold text-2xl text-center xl:text-3xl text-[#0F1035] shadow-lg xl:h-16">Manage Your Todos</div>
       <div className="w-full md:max-w-[80vw] mx-auto xl:mt-6 mt-1 lg:mt-2  shadow-lg rounded-lg xl:px-44 py-3 px-2 max-w-[100vw]">
-      
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search tasks..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
       {/* current todo starts  */}
         <div className="text-xl heading md:text-2xl lg:text-3xl text-center mb-4 lg:mb-8 mt-2 ">
           Planned Stage
@@ -24,7 +36,7 @@ function App() {
         
            <div className="flex flex-wrap gap-y-3">
             {/* Looping todos from the array of todos getting each element key is required while loopin html  */}
-              {todos.map((todo) => (
+              {filteredTodos.map((todo) => (
                 <div key={todo.id} className={`w-full`}>
                   {
                     !todo.completed ? (<Todos todo={todo} />):(null)
